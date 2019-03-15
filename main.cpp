f8ebe7dca7f0f2484edd1192ea34a679395e7fa0
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-03-14 21:36:21 -0700

    V2 that doesn't work

--- main.cpp
+++ main.cpp
@@ -8,8 +8,12 @@
 #include <cmath>
 using namespace std;
 
-vector<int> config={1,2,1};
-vector<vector<Node>> network;
+struct Node{
+    vector<float> weights;
+    float value=0;
+    float delta=0;
+};
+
 struct Test{
     vector<float> in;
     vector<float> out;
@@ -19,18 +23,60 @@ struct Test{
     }
 };
 
-struct Node{
-    vector<float> weights;
-    int value=0;
-    int delta=0;
-};
+vector<int> config={1,2,1};
+vector<vector<Node>> network;
+
+float transfer(float activation){
+    return 1/(1+exp(-activation));
+}
+
+float activate(vector<float> weights, vector<float> inputs){
+    float activation=weights[weights.size()-1];
+    for(int i=0;i<weights.size()-1;i++){
+        activation+=weights[i]*inputs[i];
+    }
+    return activation;
+}
 
 vector<float> forwardPropagate(vector<float> input){
-    
+    for(int y=0;y<network[0].size();y++){
+        network[0][y].value=transfer(input[y]);
+    }
+    for(int x=1;x<network.size();x++){
+        vector<float> newInputs;
+        for(int y=0;y<network[x].size();y++){
+//            cout << network[x][y].weights.size() << endl;
+            float activation=activate(network[x][y].weights,input);
+            network[x][y].value=transfer(activation);
+//            cout << input[0] << " results in "<<transfer(activation)<< endl;
+            newInputs.push_back(network[x][y].value);
+        }
+        input=newInputs;
+    }
+    return input;
 }
 
 vector<float> backwardPropagate(vector<float> output){
-    
+    for(int x=network.size()-1;x>=0;x--){
+        vector<float> errors;
+        if(x!=network.size()-1){
+            for(int y=0;y<network[x].size();y++){
+                float error=0;
+                for(int k=0;k<network[x+1].size();k++){
+                    error+=network[x+1][k].weights[y]*network[x+1][k].delta;
+                }
+                errors.push_back(error);
+            }
+        }else{
+            for(int y=0;y<network[x].size();y++){
+                errors.push_back(output[y]*transfer(network[x][y].value));
+            }
+        }
+        for(int y=0;y<network[x].size();y++){
+            network[x][y].delta=errors[y]*transfer(network[x][y].value);
+        }
+    }
+    return {};
 }
 
 void initNet(){
@@ -55,28 +101,69 @@ float cost(vector<float> result, vector<float> output){
     return ans;
 }
 
-void updateWeights(vector<float> input, float rate){
+void display(){
+    for(int x=0;x<network.size();x++){
+        if(x!=0){
+            cout << endl;
+            for(int y=0;y<network[x].size();y++){
+                cout << " [[ ";
+                for(int i=0;i<network[x][y].weights.size();i++){
+                    cout << network[x][y].weights[i]<<" ";
+                }
+                cout << "]] "<<"  ";
+            }
+            cout << endl;
+        }
+        for(int y=0;y<network[x].size();y++){
+            cout << "(V:"<<network[x][y].value<<", D: "<<network[x][y].delta<<")"<< " ";
+        }
+    }
+    cout << endl<<endl;
+}
 
+void updateWeights(vector<float> input, float rate){
+    for(int x=0;x<network.size();x++){
+        vector<Node> inputs=network[network.size()-1];
+        if(x!=0){
+            inputs={};
+            for(int y=0;y<network[x-1].size();y++){
+                inputs.push_back(network[x-1][y]);
+            }
+            for(int y=0;y<network[x].size();y++){
+                for(int k=0;k<inputs.size();k++){
+//                    cout << k << " in a list of " << inputs.size() << ", with coords "<< x << "," << y<<", size of weight vector is " << network[x][y].weights.size()<< endl;
+                    network[x][y].weights[k]+=rate*network[x][y].delta*inputs[k].value;
+                }
+                network[x][y].weights[network[x][y].weights.size()-1]+=rate*network[x][y].delta;
+            }
+        }
+    }
 }
 
 void train(vector<Test> dataset, float rate, int epoch){
     for(int i=0;i<epoch;i++){
         float err=0;
         for(int k=0;k<dataset.size();k++){
+            for(int j=0;j<dataset[k].in.size();j++){
+                cout << "Test Case: "<<dataset[k].in[j] << " ";
+            }
+            cout <<"= "<<dataset[k].out[0]<< endl;
             vector<float> result=forwardPropagate(dataset[k].in);
             err+=cost(result,dataset[k].out);
             backwardPropagate(dataset[k].out);
             updateWeights(dataset[k].in,0.1);
+            display();
         }
     }
 }
 
 int main(){
     vector<Test> dataset;
-    for(int i=0;i<10;i++){
-        dataset.push_back(Test({float(i)},{float(-2*i)}));
+    for(int i=0;i<100;i++){
+        float randomNum=(rand()%2);
+        dataset.push_back(Test({randomNum},{1-randomNum}));
     }
-    initNet(network);
-    train(network,dataset,0.5,100);
+    initNet();
+    train(dataset,0.1,dataset.size());
     return 0;
 }
--- main.py
+++ main.py
@@ -79,23 +79,15 @@ def train_network(network, train, l_rate, n_epoch, n_outputs):
 			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
 			backward_propagate_error(network, expected)
 			update_weights(network, row, l_rate)
+			for layer in network:
+				print(layer)
+			print("\n");
 		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
  
 # Test training backprop algorithm
 seed(1)
-dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
+dataset = [[0,1],[1,0],[0,1],[1,0],[0,1],[1,0]];
 n_inputs = len(dataset[0]) - 1
 n_outputs = len(set([row[-1] for row in dataset]))
 network = initialize_network(n_inputs, 2, n_outputs)
 train_network(network, dataset, 0.5, 20, n_outputs)
-for layer in network:
-	print(layer)
